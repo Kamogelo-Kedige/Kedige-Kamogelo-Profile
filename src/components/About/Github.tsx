77287aaf80c4f9e1850a534a8ccc6d13c0504f72
Kamogelo-Kedige <kedigekamogelo32@gmail.com>
2026-06-05 22:01:52 +0200

    fix: update GitHub username and color scheme in GitHub calendar component

--- src/components/About/Github.tsx
+++ src/components/About/Github.tsx
@@ -15,10 +15,10 @@ function Github() {
         Days I <strong className="purple">Code</strong>
       </h1>
       <GitHubCalendar
-        username="soumyajit4419"
+        username="Kamogelo-Kedige"
         blockSize={30}
         blockMargin={10}
-        color="#c084f5"
+        color="#1a659e"
         fontSize={20}
       />
     </div>
