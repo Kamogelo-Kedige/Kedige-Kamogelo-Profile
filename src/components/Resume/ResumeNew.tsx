96674129bc64de9a22519139b27cea041efc528b
Kamogelo-Kedige <kedigekamogelo32@gmail.com>
2026-06-05 21:31:09 +0200

    fix: update resume PDF to reflect new profile information

--- src/components/Resume/ResumeNew.tsx
+++ src/components/Resume/ResumeNew.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Soumyajit_Behera.pdf";
+import pdf from "../../Assets/../Assets/Profile Of Kedige Kamogelo.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
