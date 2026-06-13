8fb1a2cf9c9a695a270241ff5a643ff5d416a9b5
Kamogelo-Kedige <kedigekamogelo32@gmail.com>
2026-06-13 21:19:23 +0200

    Ennhance about page

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -13,27 +13,11 @@ function About() {
       <Particle />
       <div className="relative px-0 pb-8 pt-36 text-white">
         <div className="mx-auto max-w-7xl px-4 md:px-8">
-          <div className="grid grid-cols-1 gap-6 px-2 md:grid-cols-12 md:px-3">
-            <div className="flex flex-col justify-center py-8 md:col-span-7 md:py-12">
-              <h1 className="pb-5 text-4xl md:text-[2.1em]">
-                Know who <strong className="purple">I am</strong>
-              </h1>
-              <Aboutcard />
-            </div>
-            <div className="md:col-span-5 md:py-14 md:pt-28">
-              <img src={laptopImg} alt="about" className="h-auto w-full" />
-            </div>
-          </div>
           <h1 className="pb-2 pt-3 text-center text-4xl font-medium text-white md:text-[2.3em]">
-            Professional <strong className="purple">Skillset </strong>
+            More Tools and <strong className="purple">SkillSet </strong>
           </h1>
 
           <Techstack />
-
-          <h1 className="pb-2 pt-3 text-center text-4xl font-medium text-white md:text-[2.3em]">
-            <strong className="purple">Tools</strong> I use
-          </h1>
-          <Toolstack />
 
           <Github />
         </div>
