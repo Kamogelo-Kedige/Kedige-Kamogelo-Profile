f470bee8a10125aa43603056f12a65baaedcd8ca
Kamogelo-Kedige <kedigekamogelo32@gmail.com>
2026-07-17 15:49:40 +0200

    update AboutCard component: revise personal introduction and educational background

--- src/components/About/AboutCard.tsx
+++ src/components/About/AboutCard.tsx
@@ -1,40 +1,27 @@
 import React from "react";
-import { ImPointRight } from "react-icons/im";
 
 function AboutCard() {
   return (
     <div className="quote-card-view rounded-lg border border-white/10 p-6 text-left">
       <blockquote className="blockquote mb-0">
         <p style={{ textAlign: "justify" }}>
-          Hi everyone! I’m <span className="purple"></span> from{" "}
-          <span className="purple"></span>.
-          <br /> <span className="purple">Software Developer</span> at{" "}
-          <span className="purple">Juspay</span>.
-          <br />I hold an Integrated M.Sc. (IMSc) in{" "}
-          <span className="purple">Mathematics and Computing</span> from{" "}
-          <span className="purple">BIT Mesra</span>.
+          I’m Kamogelo Kedige, a final-year Computer Science and Informatics
+          student at the University of Johannesburg with experience building
+          full-stack applications and collaborating on real software projects.
+          I’ve earned Top Achiever recognition, UJenius Top 1% recognition, and
+          a Golden Key International Honour Society distinction during my
+          studies.
           <br />
           <br />
-          Outside of coding, I love engaging in activities that keep me creative
-          and inspired:
+          <span className="purple">Education:</span> BSc in IT in Computer
+          Science and Informatics, expected completion in 2026 at the University
+          of Johannesburg.
+          <br />
+          <span className="purple">Focus:</span> Full-stack development,
+          software engineering, databases, and web application development.
+          <br />
+          <br />
         </p>
-
-        <ul>
-          <li className="about-activity">
-            <ImPointRight /> Playing Games 🎮
-          </li>
-          <li className="about-activity">
-            <ImPointRight /> Writing Tech Blogs ✍️
-          </li>
-          <li className="about-activity">
-            <ImPointRight /> Traveling and Exploring New Places 🌍
-          </li>
-        </ul>
-
-        <p style={{ color: "rgb(155 126 172)" }}>
-          "Strive to build things that make a difference!"{" "}
-        </p>
-        <footer className="blockquote-footer">Soumyajit</footer>
       </blockquote>
     </div>
   );
